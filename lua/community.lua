ae631972acfaff9522c626559c2fec6bb922ff75
sukalov <matvey10@gmail.com>
2024-10-11 05:57:12 +0300

    add all the astronvim config with minor starter modifications

---
+++ lua/community.lua
@@ -0,0 +1,11 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
+  { import = "astrocommunity.pack.typescript" },
+  -- import/override with your plugins folder
+}
